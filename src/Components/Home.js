a08c5e1ce13695fdaafdad08937ea2730fe5c8f1
Yamin Shaikh <yaminshaikh.ys47@gmail.com>
2022-11-18 16:55:07 +0530

    add task and new columns

---
+++ src/Components/Home.js
@@ -0,0 +1,65 @@
+import { useState , useRef } from "react";
+
+const Home = () => {
+  const [column, setcolumn] = useState([]);
+  const [titlearr, settitlearr] = useState([]);
+  const [change, setchange] = useState("");
+  const inputRef = useRef()
+  const addNewCol = () => {
+    settitlearr([]);
+    const note = {
+      id: Date.now(),
+    };
+    column.push(note);
+    setcolumn([...column]);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = change;
+    titlearr.push(title);
+    settitlearr([...titlearr]);
+    inputRef.current.value = ""
+    const actualTitle = titlearr;
+    column[e.target.id] = {
+      ...column[e.target.id],
+      titles: actualTitle,
+    };
+  };
+  return (
+    <>
+      <section className="grid-layout">
+        <div className="add-note" onClick={addNewCol}>
+          Add Column
+        </div>
+        {column.length > 0
+          ? column?.map((curr, idx) => {
+              return (
+                <div className="box" key={idx} id={idx}>
+                <h2>TODO</h2>
+                  {column[idx]?.titles?.map((curr, idx) => {
+                    return (
+                      <div className="tasks" key={idx}>
+                        <p className="title">{curr}</p>
+                      </div>
+                    );
+                  })}
+                  <form onSubmit={handleSubmit} id={idx}>
+                    <input
+                      type="text"
+                      placeholder="Add Task"
+                      id={idx}
+                      onChange={(e) => setchange(e.target.value)}
+                      onBlur={() => settitlearr([])}
+                      ref={inputRef}
+                    />
+                  </form>
+                </div>
+              );
+            })
+          : ""}
+      </section>
+    </>
+  );
+};
+
+export default Home;
